04931ec467bde15c5dfd1c4fc9b5375f8ec49dfb
jil732 <52608275+jil732@users.noreply.github.com>
2020-05-19 19:00:59 +0000

    resolve #7

--- structure/fileMap.go
+++
@@ -1,7 +0,0 @@
-package structure
-
-// FileMap is the return type of NameNode.Read()
-type FileMap struct {
-	Fsize uint64   // size of the file, to handle padding
-	Nodes []string // Nodes[i] stores the addr of DataNode with ith Block
-}
