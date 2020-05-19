d9a05727868421e377f29a3855eba23d214daa68
jil732 <52608275+jil732@users.noreply.github.com>
2020-05-19 22:52:45 +0000

    work on #12 #13

--- client/README.md
+++ client/README.md
@@ -3,8 +3,8 @@
 
 ## APIs
 
-`Create(fname string, fsize uint64, rfactor int) error`
+`Create(fname string, fsize uint64, rfactor int, data []byte) error`
 
-Create a file with name, size, and replication factor
+Create a file with name, size, replication factor and content
 
 `Read(fname string) []byte`
--- client/client.go
+++ client/client.go
@@ -1,36 +1,39 @@
 package client
 
 import (
-	"encoding/binary"
-	"hash/fnv"
-	"math/rand"
-	"time"
+	"fmt"
+	"sync"
 
 	gifts "github.com/GIFTS-fs/GIFTS"
 	"github.com/GIFTS-fs/GIFTS/master"
+	"github.com/GIFTS-fs/GIFTS/storage"
 )
 
 // Client is the client of GIFTS
 type Client struct {
-	master *master.Conn
-	rand   *rand.Rand
+	master   *master.Conn
+	storages sync.Map
 }
 
 // NewClient is the constrctor for GiFTS's client
 func NewClient(masters []string) *Client {
-	h := fnv.New64a()
-	binary.Write(h, binary.LittleEndian, time.Now().UnixNano())
-	h.Write(h.Sum())
-
 	c := Client{}
-	c.conn = master.NewConn(masters[0]) // hard-code for single master
-	c.rand = rand.New(rand.NewSource(h.Sum64()))
+	c.master = master.NewConn(masters[0]) // WARN: hard-code for single master
 	return &c
+}
+
+func (c *Client) storageConn(addr string) *storage.Conn {
+	conn, ok := c.storages.Load(addr)
+	if !ok || conn == nil {
+		conn = storage.NewConn(addr)
+		c.storages.Store(addr, conn)
+	}
+	return conn.(*storage.Conn)
 }
 
 // Create a file with file name, file size, and replication factor.
 // Return error if fname already exists, network failure etc.
-func (c *Client) Create(fname string, fsize uint64, rfactor int) error {
+func (c *Client) Create(fname string, fsize uint64, rfactor int, data []byte) error {
 	assignments, err := c.master.Create(fname, fsize, rfactor)
 	if err != nil {
 		return err
@@ -41,24 +44,52 @@ func (c *Client) Create(fname string, fsize uint64, rfactor int) error {
 		nBlocks++
 	}
 
-	if nBlocks != len(assignments) {
+	// TODO: safe casting?
+	if nBlocks != uint64(len(assignments)) {
 		// TODO: remove this
 		panic("wrong number of blocks!!")
 	}
 
-	// WARN: for this system, all "files" are populated by random bytes here,
-	// no actual writes supported (yet)
-
-	var byteBlocks [][]byte
-	for i := 0; i < nBlocks; i++ {
-		blk := make([]byte, gifts.GiftsBlockSize)
-		c.rand.Read(blk)
-		byteBlocks = append(byteBlocks, blk)
-	}
-
-	for i := 0; i < nBlocks; i++ {
+	// TODO: safe casting?
+	for i := uint64(0); i < nBlocks; i++ {
+		// TODO: figure out padding and so forth
+		var b gifts.Block = data[i*gifts.GiftsBlockSize : (i+1)*gifts.GiftsBlockSize]
 		for _, rAddr := range assignments[i].Replicas {
-			// IN PROGRESS: need a connection to Storages
+			succ, err := c.storageConn(rAddr).Set(assignments[i].BlockID, b)
+			if err != nil {
+				return err
+			} else if !succ {
+				return fmt.Errorf("Set Failed for BlockID %v: %v", assignments[i].BlockID, b)
+			}
 		}
 	}
+
+	return nil
+}
+
+func (c *Client) Read(fname string) ([]byte, error) {
+	fblks, err := c.master.Read(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	if fblks.Fsize == 0 {
+		return []byte{}, nil
+	}
+
+	// get a multiple of gifts.GiftsBlockSize
+	paddingSize := fblks.Fsize % gifts.GiftsBlockSize
+	bufSize := fblks.Fsize + paddingSize
+
+	data := make([]byte, bufSize)
+	for i, assignment := range fblks.Assignments {
+		// TODO: works?????????
+		err := c.storageConn(assignment.Replicas[0]).Get(assignment.BlockID, data[i*gifts.GiftsBlockSize:])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// truncate the padding
+	return data[:bufSize-paddingSize], nil
 }
