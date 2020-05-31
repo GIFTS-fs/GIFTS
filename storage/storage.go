770d8c86af6481e8fe0e6b0150cd5110260345b7
Dhiren Lad <djayl001@aol.com>
2020-05-30 20:59:44 -0700

    Cache RPC connections and minor reformatting of error messages

--- storage/storage.go
+++ storage/storage.go
@@ -21,6 +21,7 @@ type Storage struct {
 	logger     *gifts.Logger // PRODUCTION: banish this
 	blocks     map[string]gifts.Block
 	blocksLock sync.RWMutex
+	rpc        sync.Map
 }
 
 // NewStorage creates a new storage node
@@ -82,9 +83,9 @@ func (s *Storage) Get(id string, ret *gifts.Block) error {
 
 	// Check if ID exists
 	if !found {
-		msg := fmt.Sprintf("Block with ID %s does not exist", id)
-		s.logger.Printf("Storage.Get(%q) => %q", id, msg)
-		return fmt.Errorf(msg)
+		err := fmt.Errorf("Block with ID %s does not exist", id)
+		s.logger.Printf("Storage.Get(%q) => %q", id, err)
+		return err
 	}
 
 	// Copy data
@@ -104,15 +105,15 @@ func (s *Storage) Migrate(kv *structure.MigrateKV, ignore *bool) error {
 
 	// Check if ID exists
 	if !found {
-		msg := fmt.Sprintf("Block with ID %s does not exist", kv.ID)
-		s.logger.Printf("Storage.Migrate(%q, %q) => %q", kv.ID, kv.Dest, msg)
-		return fmt.Errorf(msg)
+		err := fmt.Errorf("Block with ID %s does not exist", kv.ID)
+		s.logger.Printf("Storage.Migrate(%q, %q) => %q", kv.ID, kv.Dest, err)
+		return err
 	}
 
 	// Start an RPC session with the destination and copy the block
-	rs := NewRPCStorage(kv.Dest)
+	rs, _ := s.rpc.LoadOrStore(kv.Dest, NewRPCStorage(kv.Dest))
 	blockKV := structure.BlockKV{ID: kv.ID, Data: block}
-	if err := rs.Set(&blockKV); err != nil {
+	if err := rs.(*RPCStorage).Set(&blockKV); err != nil {
 		s.logger.Printf("Storage.Migrate(%q, %q) => %v", kv.ID, kv.Dest, err)
 		return err
 	}
@@ -130,9 +131,9 @@ func (s *Storage) Unset(id string, ignore *bool) error {
 
 	// Check if ID exists
 	if !found {
-		msg := fmt.Sprintf("Block with ID %s does not exist", id)
-		s.logger.Printf("Storage.Unset(%q) => %q", id, msg)
-		return fmt.Errorf(msg)
+		err := fmt.Errorf("Block with ID %s does not exist", id)
+		s.logger.Printf("Storage.Unset(%q) => %q", id, err)
+		return err
 	}
 
 	// Delete block
