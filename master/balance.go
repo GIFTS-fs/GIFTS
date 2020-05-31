0b3803b1850b75f0baf45f6063a115e35fc98528
jil732 <52608275+jil732@users.noreply.github.com>
2020-05-31 20:56:55 +0000

    attempt to balance

--- master/balance.go
+++ master/balance.go
@@ -1,10 +1,10 @@
 package master
 
-// periodically check the load status
-func (m *Master) balance() {
-	var toBalance []*fMeta
-
+// detectUnbalance based on the policy,
+// return a slice of fMeta that are considered unbalanced
+func (m *Master) detectUnbalance() (unbalanced []*fMeta) {
 	m.trafficLock.Lock()
+	// currentMedian must be read-only after the critical section
 	currentMedian := m.trafficMedian.Median()
 	m.trafficLock.Unlock()
 
@@ -20,16 +20,42 @@ func (m *Master) balance() {
 			return true
 		}
 
+		// Assume only balance() will change nReplica and nStorage
+		// no locks around those 2 fields
+
 		// Policy 1: reference count / number of replications > median reference count / number of storage
 		if tempature/float64(fm.nReplica) > currentMedian/float64(m.nStorage) {
 			m.logger.Printf("balance Policy 1 caught: %v", fm)
-			toBalance = append(toBalance, fm)
+			unbalanced = append(unbalanced, fm)
 		}
 
 		return true
 	})
 
-	if len(toBalance) == 0 {
-		return
+	return
+}
+
+type enlistment struct {
+	replica *storageMeta
+	blockID string
+}
+
+// enlistNewReplicas for file fm, returns a list of enlistment
+func (m *Master) enlistNewReplicas(fm *fMeta) (enlistments []*enlistment) {
+	panic(1)
+}
+
+// replicateFile fm to newReplica
+func (m *Master) replicateFile(fm *fMeta, newReplica *storageMeta) {
+
+}
+
+// periodically check the load status
+func (m *Master) balance() {
+	unbalanced := m.detectUnbalance()
+
+	for _, ub := range unbalanced {
+		_ = ub
 	}
+
 }
