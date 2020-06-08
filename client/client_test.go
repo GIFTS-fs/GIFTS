5add9b65576178694cf253e57f0d324cb153feb8
Dhiren Lad <djayl001@aol.com>
2020-06-08 01:06:45 +0000

    Update client_test to alternate between two hot files

--- client/client_test.go
+++ client/client_test.go
@@ -423,7 +423,7 @@ func TestBenchmarkClient_OneFileWave(t *testing.T) {
 	g := generate.NewGenerate()
 	fileSize := config.GiftsBlockSize
 	nReplicas := 1
-	runTime := 60
+	runTime := 120
 	nFileOneReaders := 5
 	nFileTwoReaders := 5
 
@@ -438,6 +438,8 @@ func TestBenchmarkClient_OneFileWave(t *testing.T) {
 		c.Store(fName, uint(nReplicas), data)
 	}
 
+	done := make(chan []int, nFileOneReaders+nFileTwoReaders)
+
 	// Create 5 readers for file one (read every 10ms)
 	fmt.Println("Creating readers for file one")
 	for i := 0; i < nFileOneReaders; i++ {
@@ -447,11 +449,25 @@ func TestBenchmarkClient_OneFileWave(t *testing.T) {
 			client := NewClient([]string{config.Master}, config)
 			data := make([]byte, fileSize)
 
+			nReads := 0
+			readArr := make([]int, 0, runTime)
+			timer := 1.0
+
+			defer func() { done <- readArr }()
 			for startTime := time.Now(); time.Since(startTime).Hours() < 2; {
 				data, err = client.Read(fName)
 				test.AF(t, err == nil, fmt.Sprintf("Client.Read failed: %v", err))
+				nReads++
 
-				time.Sleep(10 * time.Millisecond)
+				if time.Since(startTime).Seconds() > timer {
+					readArr = append(readArr, nReads)
+					nReads = 0
+					timer++
+				}
+
+				if timer < 60 {
+					time.Sleep(100 * time.Millisecond)
+				}
 			}
 
 			t.Log(len(data))
@@ -460,7 +476,6 @@ func TestBenchmarkClient_OneFileWave(t *testing.T) {
 
 	// Create 35 readers for file two
 	fmt.Println("Creating readers for file two")
-	done := make(chan []int, nFileTwoReaders)
 	for i := 0; i < nFileTwoReaders; i++ {
 		fName := "file_1"
 
@@ -483,6 +498,10 @@ func TestBenchmarkClient_OneFileWave(t *testing.T) {
 					nReads = 0
 					timer++
 				}
+
+				if timer >= 60 {
+					time.Sleep(100 * time.Millisecond)
+				}
 			}
 
 			t.Log(len(data))
@@ -491,7 +510,7 @@ func TestBenchmarkClient_OneFileWave(t *testing.T) {
 
 	// Wait for results
 	results := make([]int, runTime+5)
-	for i := 0; i < nFileTwoReaders; i++ {
+	for i := 0; i < nFileOneReaders+nFileTwoReaders; i++ {
 		clientResult := <-done
 
 		for i := range clientResult {
